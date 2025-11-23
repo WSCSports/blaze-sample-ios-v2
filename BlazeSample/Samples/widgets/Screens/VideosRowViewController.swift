a78aa1ca685a3016fca38039660bf8af0cbcc329
DorZafrir <dor.zafrir@wsc-sports.com>
2025-11-23 14:51:57 +0200

    Support multi-aspect ratio for the videos row widget

--- BlazeSample/Samples/widgets/Screens/VideosRowViewController.swift
+++ BlazeSample/Samples/widgets/Screens/VideosRowViewController.swift
@@ -47,6 +47,7 @@ class VideosRowViewController: BaseWidgetEditOptionsViewController {
         widget.widgetIdentifier = viewModel.currentWidgetType.rawValue
         widget.widgetDelegate = viewModel.widgetDelegate
         widget.shouldOrderWidgetByReadStatus = true
+        widget.videosPlaybackConfiguration?.multiAspectRatio = true // Allow multi aspect ratio feature on this widget
         widget.reloadData(progressType: .skeleton)
         widget.embedInView(contentView)
         self.widgetView = widget
